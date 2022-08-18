9e249ae63f95fe5c1675e96cdbf406812d98b419
Соболев Марк <mark.sobolev@cosysoft.ru>
2022-08-18 11:37:03 +0400

    initial commit

--- src/index.ts
+++ src/index.ts
@@ -6,32 +6,33 @@ const port = process.env.PORT || 3003;
 app.use(express.json());
 
 interface Entity {
-    id: number,
-    name: string
+  id: number,
+  name: string
 }
 
 const example: Record<string, Entity> = {
   // todo mock
-  ['1']:{
+  '1': {
     id: 1,
     name: 'Example',
-  } };
+  },
+};
 
 
-app.get('/', (request, response) =>{
+app.get('/', (request, response) => {
   response.json({
     a: 1,
     b: 2,
   });
 });
 
-app.get('/example', (request, response) =>{
+app.get('/example', (request, response) => {
   response.status(200).json(example);
 });
 
-app.post('/example', (request, response) =>{
+app.post('/example', (request, response) => {
   const payload = request.body;
-  if('id' in payload && 'name' in payload){
+  if ('id' in payload && 'name' in payload) {
     example[payload.id] = payload;
     response.status(201).json(payload);
     return;
@@ -39,9 +40,9 @@ app.post('/example', (request, response) =>{
   response.sendStatus(400);
 });
 
-app.delete('/example/:id', (request, response) =>{
-  const { id } = request.params;
-  if(id in example) {
+app.delete('/example/:id', (request, response) => {
+  const {id} = request.params;
+  if (id in example) {
     delete example[id];
     response.sendStatus(200);
     return;
@@ -49,19 +50,19 @@ app.delete('/example/:id', (request, response) =>{
   response.sendStatus(404);
 });
 
-app.get('/example/:id', (request, response) =>{
-  const { id } = request.params;
-  if(id in example) {
+app.get('/example/:id', (request, response) => {
+  const {id} = request.params;
+  if (id in example) {
     response.status(200).json(example[id]);
     return;
   }
   response.sendStatus(404);
 });
 
-app.put('/example', (request, response) =>{
+app.put('/example', (request, response) => {
   const payload = request.body;
-  if('id' in payload && 'name' in payload){
-    if(example[payload.id]){
+  if ('id' in payload && 'name' in payload) {
+    if (example[payload.id]) {
       (example[payload.id] as Entity).name = payload.name;
       response.status(200).json(example[payload.id]);
       return;
